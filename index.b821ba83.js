0ffe6ee0aaee2b38bd60025209d84fa1dd3de33e
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-02-16 11:41:48 +0200

    make build

---
+++ index.b821ba83.js
@@ -0,0 +1,3 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".gallery__thumb");if(r){var a=r.closest("a");a&&(e.src=a.href)}});//# sourceMappingURL=index.b821ba83.js.map
+
+//# sourceMappingURL=index.b821ba83.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const largeImg = document.getElementById("largeImg");
+const ul = document.getElementById("thumbs");
+ul.addEventListener("click", (evt)=>{
+    evt.preventDefault();
+    const item = evt.target.closest(".gallery__thumb");
+    if (!item) return;
+    const link = item.closest("a");
+    if (!link) return;
+    largeImg.src = link.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
